9f6bd3923b40eb2613bbb44718ab84afff8ceef3
BT_1N3_12 <sbajnath6151@bths.edu>
2019-02-15 08:39:30 -0500

    dd

--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -4,6 +4,9 @@ package Activity1;
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    public static void main{
+
+    }
     Card test = new Card("Ace","Hearts",1);
     Card test2 = new Card("Queen", "Spades",12);
     Card test3 = new Card("7", "Diamonds",7);
